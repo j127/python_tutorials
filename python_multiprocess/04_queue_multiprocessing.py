73d348faf5508d88f0cc65f184d2a43a3da0acbc
J. Cohen <hostelmarketing@gmail.com>
2018-06-22 01:44:57 -0700

    Finish video 4

--- python_multiprocess/04_queue_multiprocessing.py
+++ python_multiprocess/04_queue_multiprocessing.py
@@ -1,45 +1,27 @@
-"""An example of sharing memory with multiprocessing.
+"""An example of `multiprocessing.Queue`.
 
-In the other multiprocessing example, the results array wasn't visible
-outside of the process. This example uses shared memory to fix that.
+There is also a `queue` module for in-process memory. It's for sharing
+data between threads.
 """
 import multiprocessing
 
 
-def calc_square(ns, result, v):
-    """This example uses a `result` arg rather than a global var."""
-    print('incoming v is {}'.format(v.value))
-    v.value = 12  # changing the shared variable here \˚ㄥ˚\
-    for idx, n in enumerate(ns):
-        # The `append` method isn't available on `result` in this
-        # version.
-        result[idx] = round(n * n, 2)
-
-    # See below for why this prints strange output
-    print('inside process: {}'.format(result))
+def calc_square(ns, q):
+    """The child process."""
+    for n in ns:
+        # `q` is a queue (FIFO).
+        q.put(round(n * n, 2))
 
 
 if __name__ == '__main__':
     nums = [round(pow(1.27, n+1), 2) for n in range(10)]
 
-    # This `result` is shared memory.
-    # Example: `i` for integer or `d` for double
-    # `10` because the size of the array is 10
-    result = multiprocessing.Array('d', 10)
-
-    # Send another variable into the process (`i` for int)
-    v = multiprocessing.Value('i', 11)
-
-    # Pass the shared memory into the `args` tuple here:
-    p = multiprocessing.Process(target=calc_square, args=(nums, result, v))
+    q = multiprocessing.Queue()
+    p = multiprocessing.Process(target=calc_square, args=(nums, q))
 
     p.start()
     p.join()
 
-    # The `[:]` syntax is to print the elements of the array. If
-    # left off, it will print something more like <SynchronizedArray
-    # wrapper for <multiprocessing.sharedctypes.c_double_Array_10 object at
-    # 0x7f5dbf62c730>>
-    print('outside process: {}'.format(result[:]))
-
-    print('the shared var `v` is now {}'.format(v.value))
+    while q.empty() is False:
+        # Get items from the queue
+        print(q.get())
